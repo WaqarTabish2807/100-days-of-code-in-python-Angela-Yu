48df4cdf3d30c41cae59ba52891d4b795bf5c4b5
Md Waqar Tabish <85379681+WaqarTabish2807@users.noreply.github.com>
2022-03-29 21:08:37 +0530

    Add files via upload

---
+++ Day018_Turtle-graphics/main.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen
+import turtle
+
+def move():
+    shiwi_the_turtle.left(72)
+    shiwi_the_turtle.forward(100)
+
+
+shiwi_the_turtle = turtle
+shiwi_the_turtle.forward(100)
+move()
+move()
+move()
+move()
+
+
+screen = Screen()
+screen.exitonclick()
